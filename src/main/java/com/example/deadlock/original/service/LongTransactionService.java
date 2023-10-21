016576fce22daec2b2f4e2cba182b3dd50ec134e
이중석 <ejoongseok@gmail.com>
2023-10-21 15:22:51 +0900

    STEP-3 | CQS 분리. (#3)
    Co-authored-by: 이중석/백엔드개발팀 <jslee@ktown4u.com>

--- src/main/java/com/example/deadlock/original/service/LongTransactionService.java
+++ src/main/java/com/example/deadlock/original/service/LongTransactionService.java
@@ -3,7 +3,6 @@ package com.example.deadlock.original.service;
 import com.example.deadlock.original.component.DooComponent;
 import com.example.deadlock.original.component.DooHandler;
 import com.example.deadlock.original.component.FooComponent;
-import com.example.deadlock.original.component.UpdateComponent;
 import com.example.deadlock.original.domain.BaseEntity;
 import com.example.deadlock.original.domain.Child;
 import com.example.deadlock.original.domain.ChildRepository;
@@ -19,7 +18,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -27,7 +28,6 @@ public class LongTransactionService {
 
     private final ChildRepository childRepository;
     private final RootRepository rootRepository;
-    private final UpdateComponent updateComponent;
     private final ExternalApi externalApi;
     private final FooComponent fooComponent;
     private final DooComponent dooComponent;
@@ -40,23 +40,36 @@ public class LongTransactionService {
         if (base.isAlreadyCompleted()) return;
         final String code = base.getCode();
         final Child child = getChild(code);
-        base.setData(child.getData());
+        final RootEntity rootEntity = rootRepository.findById(base.getRootId()).orElseThrow();
         final List<Foo> fooList = fooComponent.list(base.getFoo());
+        final List<Doo> dooAllList = new ArrayList<>();
         for (final Foo foo : fooList) {
             final List<Doo> dooList = dooComponent.list(foo.getId());
-            for (final Doo doo : dooList) {
-                final Long id = doo.getId();
-                final Jo jo = joRepository.findById(id).orElseThrow(() -> new RuntimeException("not found"));
-                jo.handle(doo.getJoData());
+            dooAllList.addAll(dooList);
+        }
+        final List<JoDoo> joDooList = new ArrayList<>();
+        for (final Doo doo : dooAllList) {
+            final Long id = doo.getId();
+            final Jo jo = joRepository.findById(id).orElseThrow(() -> new RuntimeException("not found"));
+            joDooList.add(new JoDoo(jo, doo.getJoData()));
+        }
 
-                doo.cool();
-            }
-            dooList.forEach(doo -> dooHandler.handle(doo));
+        base.setData(child.getData());
+        for (final JoDoo joDoo : joDooList) {
+            final Jo jo = joDoo.jo();
+            jo.handle(joDoo.joData());
+        }
+        for (final Doo doo : dooAllList) {
+            doo.cool();
+        }
+        for (final Doo doo : dooAllList) {
+            dooHandler.handle(doo);
+        }
+        for (final Foo foo : fooList) {
             foo.complete();
         }
         externalApi.call(base, child);
         base.complete();
-        final RootEntity rootEntity = rootRepository.findById(base.getRootId()).orElseThrow();
         rootEntity.complete();
         rootRepository.save(rootEntity);
         externalApi2.call(rootEntity.getId());
@@ -66,4 +79,43 @@ public class LongTransactionService {
         return childRepository.findByCode(code).orElseThrow(() -> new RuntimeException("not found"));
     }
 
+    private static final class JoDoo {
+        private final Jo jo;
+        private final Object joData;
+
+        private JoDoo(final Jo jo, final Object joData) {
+            this.jo = jo;
+            this.joData = joData;
+        }
+
+        public Jo jo() {
+            return jo;
+        }
+
+        public Object joData() {
+            return joData;
+        }
+
+        @Override
+        public boolean equals(final Object obj) {
+            if (obj == this) return true;
+            if (null == obj || obj.getClass() != getClass()) return false;
+            final var that = (JoDoo) obj;
+            return Objects.equals(jo, that.jo) &&
+                    Objects.equals(joData, that.joData);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(jo, joData);
+        }
+
+        @Override
+        public String toString() {
+            return "JoDoo[" +
+                    "jo=" + jo + ", " +
+                    "joData=" + joData + ']';
+        }
+
+    }
 }

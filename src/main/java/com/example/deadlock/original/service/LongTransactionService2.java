11b04d133d2a4c0dfc6b72540977f8dcd539b8dd
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-10-21 15:09:54 +0900

    copy & paste.

---
+++ src/main/java/com/example/deadlock/original/service/LongTransactionService2.java
@@ -0,0 +1,45 @@
+package com.example.deadlock.original.service;
+
+import com.example.deadlock.original.component.CompleteComponent;
+import com.example.deadlock.original.component.UpdateComponent;
+import com.example.deadlock.original.domain.BaseEntity;
+import com.example.deadlock.original.domain.Child;
+import com.example.deadlock.original.domain.ChildRepository;
+import com.example.deadlock.original.domain.RootEntity;
+import com.example.deadlock.original.domain.RootRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class LongTransactionService2 {
+
+    private final ChildRepository childRepository;
+    private final RootRepository rootRepository;
+    private final UpdateComponent updateComponent;
+    private final CompleteComponent completeComponent;
+
+    @Transactional
+    public void execute(final BaseEntity base) {
+        if (base.isAlreadyCompleted()) return;
+        final String code = base.getCode();
+        final Child child = getChild(code);
+        base.setData(child.getData());
+        completeComponent.execute(base, child);
+        base.complete();
+        updateRoot(base.getRootId());
+    }
+
+    private Child getChild(final String code) {
+        return childRepository.findByCode(code).orElseThrow(() -> new RuntimeException("not found"));
+    }
+
+    private void updateRoot(final Long rootId) {
+        final RootEntity rootEntity = rootRepository.findById(rootId).orElseThrow();
+        rootEntity.complete();
+        updateComponent.execute(rootEntity);
+
+    }
+
+}
